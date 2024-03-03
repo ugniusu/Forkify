9c421b31e211ea4f4651361fbbc28b7a570db28b
ugniususc <ugniususc@gmail.com>
2024-03-03 18:26:20 +0200

    Project forkify

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5c69c666-c9cd-4512-9f91-0a2fbb0b7889';
+export const MODAL_CLOSE_SECONDS = 2.5;
